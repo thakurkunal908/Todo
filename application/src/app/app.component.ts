9ac3172f0f4bb5ca431732733e9245f60230b3e9
Kunal Thakur <kunalthakur@Kunals-MacBook-Air.local>
2024-01-07 19:43:48 +0530

    Created Dialog Box to Show and Edit Note

--- application/src/app/app.component.ts
+++ application/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
 import { Note } from './interfaces/note-interface';
 import { NotesService } from './service/notes.service';
 
@@ -8,9 +8,16 @@ import { NotesService } from './service/notes.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
+  @ViewChild('dialogContainer', { read: ViewContainerRef })
+  dialogContainer!: ViewContainerRef;
+
   noteList: Note[] = [];
 
   isAddNote: boolean = false;
+
+  showDialog = false;
+
+  selectedNote!: Note;
 
   ngOnInit(): void {
     this.fetchNotes();
@@ -33,5 +40,14 @@ export class AppComponent implements OnInit {
 
   onAddNote() {
     this.isAddNote = true;
+  }
+
+  onSelectNote(selectedNote: Note) {
+    this.showDialog = true;
+    this.selectedNote = selectedNote;
+  }
+
+  onCloseModal() {
+    this.showDialog = false;
   }
 }
