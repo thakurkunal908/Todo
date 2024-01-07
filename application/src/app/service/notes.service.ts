b98acea7b85758320c1dd0312cb91a0adaed88bc
Kunal Thakur <kunalthakur@Kunals-MacBook-Air.local>
2024-01-07 16:22:13 +0530

    Create and Store note in local storage

--- application/src/app/service/notes.service.ts
+++ application/src/app/service/notes.service.ts
@@ -2,13 +2,24 @@ import { Injectable } from '@angular/core';
 import { Note } from '../interfaces/note-interface';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 export class NotesService {
+  localStorageKey = 'notes-app';
 
-    constructor() { }
+  constructor() {}
 
-    fetchAllNotes(): Note[] | [] {
-        return JSON.parse(localStorage.getItem('notes')!) || [];
-    }
+  createNote(note: Note): void {
+    const notes: Note[] = this.fetchAllNotes();
+    notes.push(note);
+    this.saveNotes(notes);
+  }
+
+  fetchAllNotes(): Note[] | [] {
+    return JSON.parse(localStorage.getItem(this.localStorageKey)!) || [];
+  }
+
+  private saveNotes(notes: any[]): void {
+    localStorage.setItem(this.localStorageKey, JSON.stringify(notes));
+  }
 }
